f993699b7e8b30c41e18d3bbe801843475fe851d
jaqueline nunes <jaqueline.nunes@insidesistemas.com.br>
2024-03-09 16:31:40 -0300

    Projeto de teste de FeatureIDE Antenna

---
+++ src/HelloWord.java
@@ -0,0 +1,24 @@
+//Annotative SPL
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+
+		// #if Wonderfull | Beautiful
+//@		System.out.print(" very");
+		// #endif
+		
+		// #if Wonderfull
+//@		System.out.print(" wonderfull");
+		// #endif
+		
+		// #if Beautiful
+//@		System.out.print(" beautifull");
+		// #endif
+		
+		// #if World
+		System.out.print(" workd");
+		// #endif
+	}
+	
+}
